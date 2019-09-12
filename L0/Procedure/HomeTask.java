2234211530db7e4d9e863401dfcada6a03dbb448
cyrilasamoah <cyrilasamoah>
2019-09-12 13:42:38 +0800

    first commit

--- L0/Procedure/HomeTask.java
+++ L0/Procedure/HomeTask.java
@@ -1,4 +1,11 @@
 package L0.Procedure;
-
 public class HomeTask {
+    public static void main(String[] args) {
+        int x =2;
+        int number=2;
+        int pow=4;
+        
+System.out.println(x);
+
+    }
 }
