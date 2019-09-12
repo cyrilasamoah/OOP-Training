db9e9228895df7f3f46c510067935894bd43b80f
cyrilasamoah <cyrilasamoah>
2019-09-12 14:41:56 +0800

    second commit

--- L0/Procedure/HomeTask.java
+++ L0/Procedure/HomeTask.java
@@ -1,11 +1,22 @@
+
 package L0.Procedure;
+import java.util.Scanner;
 public class HomeTask {
+    private static int i;
+
     public static void main(String[] args) {
-        int x =2;
-        int number=2;
-        int pow=4;
+        int x;
+        Scanner X= new Scanner(System.in);
+        int n = X.nextInt();
+        int=1;
+        double sum=0;
+        while (i<=n);
+        sum=sum+Math.pow(i,4);
+
         
-System.out.println(x);
+        
+
+
 
     }
 }
